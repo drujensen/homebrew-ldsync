74afeb435c00db1b25bdb84abf58d2809335a281
Dru Jensen <drujensen@gmail.com>
2022-03-12 10:28:02 -0800

    bump version

--- Formula/ldsync.rb
+++ Formula/ldsync.rb
@@ -1,8 +1,8 @@
 class Ldsync < Formula
   desc "ldsync utility will manage your Launch Darkly flags"
   homepage "https://github.com/drujensen/ldsync"
-  url "https://github.com/drujensen/ldsync/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "c11e3fbcd8438e20d96368c5b60f93be639982c339e3db32015dfb92f6e91503"
+  url "https://github.com/drujensen/ldsync/archive/refs/tags/v0.1.1.tar.gz"
+  sha256 "00c6c0f178b8b9c083eeb32078b15f67f50adffb07ad45e86ea1f2501cbd16b5"
 
   depends_on "crystal"
 
